b5fff58ab057143a1222605387cc351706aaf174
kwb130212-discord <kwb130212@gmail.com>
2026-08-25 19:31:27 +0900

    feat: add optional boot receiver

---
+++ app/src/main/java/com/kwb130212/rbxm/BootReceiver.kt
@@ -0,0 +1,17 @@
+package com.kwb130212.rbxm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            ContextCompat.startForegroundService(
+                context,
+                Intent(context, MacroForegroundService::class.java)
+            )
+        }
+    }
+}
